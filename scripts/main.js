8693b2064de53b216d3908bb534e01bddd76cba3
cesarlcdiaz <cesarlcdiaz.com>
2018-03-09 18:05:47 -0800

    Conditional statements to control the flow of the game

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+
+
+alert("Its the zombie apocalypse. You are looting a store and suddenly a zombie burst through the door!");
+
+const weapon = prompt("You are searching for a weapon. What do you choose? Knife, Axe, Frypan?");
+
+const randomNumber = Math.round(Math.random());
+
+alert("You attack the zombie with your" + " " + weapon);
+
+if(randomNumber === 0) {
+	alert("The zombie bites you. You Die!");
+}else{
+	alert("You kill the zombie with your" + " " + ". You Win");
+}
