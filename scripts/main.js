325db80ef3b709a53c622039ead9954ba7938812
cesarlcdiaz <cesarlcdiaz.com>
2018-03-12 19:46:25 -0700

    This added another conditional statement

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,34 @@
 
-
 alert("Its the zombie apocalypse. You are looting a store and suddenly a zombie burst through the door!");
 
 const weapon = prompt("You are searching for a weapon. What do you choose? Knife, Axe, Frypan?");
-
-//const dessision = prompt("Do you want to run? or stay and fight?");
 
 const randomNumber = Math.round(Math.random());
 
 alert("You attack the zombie with your" + " " + weapon);
 
 if(randomNumber === 0) {
+
 	alert("The zombie bites you. You Die!");
-}else{
+
+}else if(randomNumber === 1){
+
 	alert("You kill the zombie with your" + " " + ". You Win");
+
+}
+
+
+
+const safebox = alert("You have found a safe box. Click for a chance to open the box!");
+
+const lockPick = Math.round(Math.random());
+
+if(lockPick === 8) {
+
+	alert("Try again. There is something clicking!");
+
+}else if(lockPick === 1){
+
+	alert("You have open the box!");
+
 }
